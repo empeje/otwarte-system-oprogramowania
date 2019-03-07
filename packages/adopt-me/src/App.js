81ffd4e24658e0134da21a5e23ff78689f9bc95f
mpj <11813607+empeje@users.noreply.github.com>
2019-03-07 22:47:59 +0700

    Use JSX

--- packages/adopt-me/src/App.js
+++ packages/adopt-me/src/App.js
@@ -7,29 +7,15 @@ class App extends React.Component {
     alert("you clicked the title");
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "bird",
-        breed: "Cocktiel"
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "cat",
-        breed: "Mixed"
-      })
-    ]);
+    return (
+      <div>
+        <h1 onClick={this.handleTitleClick}>Adopt me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Pepper" animal="bird" breed="Cocktiel" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </div>
+    );
   }
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
