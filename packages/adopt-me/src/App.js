2deea078cb34139496eadba31f7e7bb6aebc755d
mpj <11813607+empeje@users.noreply.github.com>
2019-03-07 22:58:50 +0700

    Add more eslint

--- packages/adopt-me/src/App.js
+++ packages/adopt-me/src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt me!</h1>
+        <h1>Adopt me!</h1>
         <Pet name="Luna" animal="dog" breed="Havanese" />
         <Pet name="Pepper" animal="bird" breed="Cocktiel" />
         <Pet name="Doink" animal="cat" breed="Mixed" />
