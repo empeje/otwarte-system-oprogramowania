6d4b6196883f127e0200115e7133dd538b42fa0f
mpj <11813607+empeje@users.noreply.github.com>
2019-03-08 00:13:58 +0700

    Add more data to pet component

--- packages/adopt-me/src/App.js
+++ packages/adopt-me/src/App.js
@@ -51,6 +51,8 @@ class App extends React.Component {
                 animal={pet.animal}
                 name={pet.name}
                 breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
               />
             );
           })}
