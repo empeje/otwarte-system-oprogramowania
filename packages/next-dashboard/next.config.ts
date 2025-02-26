acab88a19049538913c3aa693c8b0ed2fa29d495
empeje <11813607+empeje@users.noreply.github.com>
2025-02-26 11:27:25 +0700

    Use turbo only on dev

--- packages/next-dashboard/next.config.ts
+++ packages/next-dashboard/next.config.ts
@@ -1,15 +1,32 @@
-import { NextConfig } from "next";
+import {NextConfig} from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
+// use turbo on dev only
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
+  experimental: isDev
+    ? {
+      turbo: {
+        rules: {
+          "*.svg": {
+            loaders: ["@svgr/webpack"],
+            as: "*.js",
+          },
         },
       },
-    },
+    }
+    : {},
+
+  webpack(config) {
+    if (!isDev) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+    }
+    return config;
   },
 };
 
