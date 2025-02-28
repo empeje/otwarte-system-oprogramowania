ec8268c1506ec018e89adacf9ca771ad745bb980
empeje <11813607+empeje@users.noreply.github.com>
2025-02-28 14:30:54 +0700

    Custom page not found

---
+++ packages/next-dashboard/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Container from "@/components/page/container";
+import AlertInfo from "@/components/alert/info";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Container className="sm:flex sm:items-start">
+        <AlertInfo
+          title={"Oops!"}
+          content={"Page not found!"}/>
+      </Container>
+    </div>
+  )
+}
